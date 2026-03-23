70262697b90483f6d8e0fe9a2e065228798b7578
cccmmm5858-cpu <cccmmm5858@gmail.com>
2026-03-23 14:54:37 +0300

    Create server.js

---
+++ server.js
@@ -0,0 +1,42 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+
+const { getMarketData } = require('./services/market');
+const { runStrategy } = require('./services/strategy');
+const { executeTrade } = require('./services/trader');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+let lastTrade = null;
+
+app.get('/', (req, res) => {
+    res.send('Trading Bot Running 🚀');
+});
+
+app.get('/run', async (req, res) => {
+    const data = await getMarketData();
+    const decision = runStrategy(data);
+
+    const trade = executeTrade(decision, data.price);
+
+    lastTrade = trade;
+
+    res.json({
+        market: data,
+        decision,
+        trade
+    });
+});
+
+app.get('/status', (req, res) => {
+    res.json({
+        lastTrade
+    });
+});
+
+app.listen(3000, () => {
+    console.log('Bot running on port 3000');
+});
